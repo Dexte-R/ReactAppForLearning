a3d79525ef54dd667176767c682b78f5319c633d
Dexter Huang <dexterhuang4577@gmail.com>
2020-11-02 15:40:16 +0800

    added feature to fetch data from springboot server on localhost

--- src/App.js
+++ src/App.js
@@ -27,13 +27,25 @@ function App() {
     setTodos(newTodos)
   }
 
+  async function getTodoFromSpringBoot() {
+    const id = refTodo.current.value  
+    if (id === '') return null
+    let todoItemFromServer = await fetch("http://localhost:8090/" + id).then(res => res.json()).then(res => {return res})
+    setTodos(currentTodos => {
+      return [...currentTodos, { id: uuid(), name: todoItemFromServer.bodyText, complete: false}]
+    })
+    refTodo.current.value = null
+  }
+
   return (
     <div>
       <TodoList todoList={todos} toggleTodo={toggleTodo} />
       <input ref={refTodo} type="text" />
       <button onClick={handleAddTodo}>Add Todo</button>
+      <button onClick={getTodoFromSpringBoot}>Add Todo From SpringBoot</button>
       <button onClick={removeCompleted}>Clear completed</button>
       <div>{todos.filter(todo => !todo.complete).length} left to do</div>
+      <div>Additional todo item fetched from SpringBoot server</div>
     </div>
   );
 }
